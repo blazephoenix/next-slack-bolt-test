f327cdaabee4e67d40798031b17bcbea4fac4035
blazephoenix <naik.tanmay283@gmail.com>
2022-07-29 14:42:16 +0530

    init

--- pages/api/[[...route]].ts
+++ pages/api/[[...route]].ts
@@ -1,9 +1,9 @@
-import { App } from '@slack/bolt';
-import { NextApiRequest, NextApiResponse } from 'next';
-import NextConnectReceiver from 'utils/NextConnectReceiver';
+import { App } from "@slack/bolt";
+import { NextApiRequest, NextApiResponse } from "next";
+import NextConnectReceiver from "utils/NextConnectReceiver";
 
 const receiver = new NextConnectReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET || 'invalid',
+  signingSecret: process.env.SLACK_SIGNING_SECRET || "invalid",
   // The `processBeforeResponse` option is required for all FaaS environments.
   // It allows Bolt methods (e.g. `app.message`) to handle a Slack request
   // before the Bolt framework responds to the request (e.g. `ack()`). This is
@@ -15,23 +15,25 @@ const receiver = new NextConnectReceiver({
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   receiver: receiver,
-  developerMode: false,
+  developerMode: true,
+  redirectUri: 'http://localhost:3000/api'
 });
 
-app.event('message', async ({ event, say }) => {
-  const text = (event as any).text;
-  say({
-    text: text || 'Hello world!',
-  });
-});
+// app.event("message", async ({ event, say }) => {
+//   const text = (event as any).text;
+//   say({
+//     text: text || "Hello world!",
+//   });
+// });
 
 // this is run just in case
 const router = receiver.start();
 
-router.get('/api', (req: NextApiRequest, res: NextApiResponse) => {
+router.get("/api", (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.query.code)
   res.status(200).json({
     test: true,
   });
-})
+});
 
 export default router;
